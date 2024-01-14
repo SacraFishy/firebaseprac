02360197a775385d149b54b0654a993197b5b851
Jpisnice <108455222+Jpisnice@users.noreply.github.com>
2024-01-14 16:53:37 +0530

    All is well

---
+++ config.js
@@ -0,0 +1,29 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDRtUTGzFit9Mvfn9Wi3RX6aZKrKCqeloU",
+    authDomain: "practical-app-a8f98.firebaseapp.com",
+    projectId: "practical-app-a8f98",
+    storageBucket: "practical-app-a8f98.appspot.com",
+    messagingSenderId: "978621724223",
+    appId: "1:978621724223:web:69d29f85a5c5843d38ea8a"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // firebase deploy --only hosting:pracapp-43cc
+
+//   {
+//   "hosting": {
+//     "site": "pracapp-43cc",
+
+//     "public": "public",
+//     ...
+//   }
+// }
